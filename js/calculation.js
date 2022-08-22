2ff1c131ba7652036708c1fc70675363375ca990
= <rakib.ivan100@gmail.com>
2022-08-22 15:41:36 +0600

    final push

--- js/calculation.js
+++ js/calculation.js
@@ -12,3 +12,22 @@ document.getElementById('calculate').addEventListener('click', function () {
     }
     setTextElementById('expense', totalExpense);
 });
+
+document.getElementById('calculateTotal').addEventListener('click', function () {
+    const playerInputValue = getElementById('playerInput');
+    const coachInputValue = getElementById('coachInput');
+    const managerInputValue = getElementById('managerInput');
+    const selectedPlayerNumber = getTextById('heading');
+    const expense = getTextById('expense');
+
+    // let totalExpense = 1;
+    if (selectedPlayerNumber === 6) {
+        totalExpense = playerInputValue * (selectedPlayerNumber - 1);
+    }
+    else {
+        totalExpense = playerInputValue * (selectedPlayerNumber);
+    }
+
+    const total = totalExpense + coachInputValue + managerInputValue;
+    setTextElementById('totalExpense', total);
+});
